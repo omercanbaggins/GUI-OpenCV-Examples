360cd7ebe42a2fa4ffc2704149cdbcc0bbd91b85
Ömer Can Demirci <68750887+omercanbaggins@users.noreply.github.com>
2026-03-05 12:18:01 +0300

    Seperate Image and QMainWindow classes are added
    In my first proper example i created two class to handle opencv and pyside 6 integration. User can stop&start video using buttons and investigate different processed outputs of same image by pressing next and prev buttons

---
+++ image.py
@@ -0,0 +1,27 @@
+import cv2
+import numpy as np
+class image:
+    def __init__(self,videoPath):
+        self.setPath(videoPath)
+        self.cvImage = np.ones((900,900,3),np.uint8)
+        self.images = [(self.cvImage)] 
+    def setPath(self,path):
+        self.videopPath = path
+        self.cap = cv2.VideoCapture(path)
+    def getNumberofVideo(self):
+        return self.images.__len__()
+    def processImage(self):
+        b,frame = self.cap.read()
+        if (b):
+            self.cvImage = cv2.resize(frame,(1280,720))
+            grayScale = cv2.cvtColor(self.cvImage, cv2.COLOR_RGB2GRAY)
+            blurredImage = cv2.GaussianBlur(grayScale,(5,5),5)
+            _,thresh = cv2.threshold(blurredImage,127,255,cv2.THRESH_BINARY)
+            cannyImage = cv2.Canny(thresh,150,50)
+            self.images = []
+            self.images.append(self.cvImage)
+            self.images.append(grayScale)
+            self.images.append(cannyImage)
+
+            return cannyImage
+        
